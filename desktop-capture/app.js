5a55a3881f3fb07ce7573d1377a5816feb3434cf
batman <salat@batmans-Mac-mini.local>
2018-03-27 18:01:17 +0300

    desktop capture

---
+++ desktop-capture/app.js
@@ -0,0 +1,141 @@
+// const {desktopCapturer} = require('electron')
+// const fs = require('fs')
+// const MediaStreamRecorder = require('msr')
+
+var streamRecorder
+let desktopSharing = false
+let localStream
+var blobs = []
+
+// function refresh () {
+//   window.$('select').imagepicker({
+//     hide_select: true
+//   })
+// }
+//
+// function addSource (source) {
+//   window.$('select').append(window.$('<option>', {
+//     value: source.id.replace(':', ''),
+//     text: source.name
+//   }))
+//   window.$('select option[value="' + source.id.replace(":", "") + '"]').attr('data-img-src', source.thumbnail.toDataURL())
+//   refresh()
+// }
+//
+// function showSources () {
+//   desktopCapturer.getSources({ types:['window', 'screen'] }, function(error, sources) {
+//     addSource(sources[1])
+//     // for (let source of sources) {
+//     //   console.log('Name: ' + source.name)
+//     //   addSource(source[1])
+//     // }
+//   })
+// }
+
+function toggle () {
+  onAccessApproved()
+  // if (!desktopSharing) {
+  //   var id = (window.$('select').val()).replace(/window|screen/g, function(match) { return match + ':' })
+  //   onAccessApproved(id)
+  // } else {
+  //   desktopSharing = false
+  //
+  //   if (localStream) {
+  //     localStream.getTracks()[0].stop()
+  //   }
+  //   streamRecorder.stop()
+  //   localStream = null
+  //
+  //   document.querySelector('button').innerHTML = 'Enable Capture'
+  //
+  //   window.$('select').empty()
+  //   showSources()
+  //   refresh()
+  // }
+}
+
+function onAccessApproved (desktopId) {
+  // if (!desktopId) {
+  //   console.log('Desktop Capture access rejected.')
+  //   return
+  // }
+  // console.log(desktopId)
+  desktopSharing = true
+  document.querySelector('button').innerHTML = 'Disable Capture'
+  // console.log('Desktop sharing started.. desktopId:' + desktopId)
+  navigator.webkitGetUserMedia({
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: 'desktop',
+        chromeMediaSourceId: 'screen:188953530',
+        minWidth: 1280,
+        maxWidth: 1280,
+        minHeight: 720,
+        maxHeight: 720
+      }
+    }
+  }, gotStream, getUserMediaError)
+
+  function gotStream (stream) {
+    localStream = stream
+    blobs = []
+    streamRecorder = new window.MediaRecorder(stream)
+    streamRecorder.ondataavailable = function (event) {
+      blobs.push(event.data)
+      stopRecording()
+    }
+    streamRecorder.start()
+    // document.querySelector('video').src = URL.createObjectURL(stream)
+    stream.onended = function () {
+      if (desktopSharing) {
+        toggle()
+      }
+    }
+  }
+
+  function getUserMediaError (e) {
+    console.log('getUserMediaError: ' + JSON.stringify(e, null, '---'))
+  }
+}
+
+function stopRecording () {
+  toArrayBuffer(new Blob(blobs, {type: 'video/webm'}), function (ab) {
+    var buffer = toBuffer(ab)
+    // var file = `./videos/example.webm`
+    console.log(buffer)
+    // fs.writeFile(file, buffer, function (err) {
+    //   if (err) {
+    //     console.error('Failed to save video ' + err)
+    //   } else {
+    //     console.log('Saved video: ' + file)
+    //   }
+    // })
+  })
+}
+function toArrayBuffer (blob, cb) {
+  let fileReader = new FileReader()
+  fileReader.onload = function () {
+    let arrayBuffer = this.result
+    cb(arrayBuffer)
+  }
+  fileReader.readAsArrayBuffer(blob)
+}
+
+function toBuffer (ab) {
+  let buffer = Buffer.alloc(ab.byteLength)
+  let arr = new Uint8Array(ab)
+  for (let i = 0; i < arr.byteLength; i++) {
+    buffer[i] = arr[i]
+  }
+  return buffer
+}
+
+window.$(document).ready(function () {
+  // showSources()
+  // refresh()
+})
+
+document.querySelector('button').addEventListener('click', function (e) {
+  toggle()
+})
--- renderer.js
+++ renderer.js
@@ -1,3 +1,39 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const {desktopCapturer} = require('electron')
+
+desktopCapturer.getSources({types: ['window', 'screen']}, (error, sources) => {
+  if (error) throw error
+  for (let i = 0; i < sources.length; ++i) {
+    if (sources[i].name === 'Electron') {
+      navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+          mandatory: {
+            chromeMediaSource: 'desktop',
+            chromeMediaSourceId: sources[i].id,
+            minWidth: 1280,
+            maxWidth: 1280,
+            minHeight: 720,
+            maxHeight: 720
+          }
+        }
+      })
+      .then((stream) => handleStream(stream))
+      .catch((e) => handleError(e))
+      return
+    }
+  }
+})
+
+function handleStream (stream) {
+  console.log(stream)
+  const video = document.querySelector('video')
+  video.srcObject = stream
+  video.onloadedmetadata = (e) => video.play()
+}
+
+function handleError (e) {
+  console.error(e)
+}
